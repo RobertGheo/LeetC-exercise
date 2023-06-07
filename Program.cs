acc14cf588707f62293f71bf0ebaf64bbd673280
RobertGheo <robert.bradu@yahoo.com>
2023-06-07 22:07:01 +0300

    upload project initiation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello there \n");
+Console.WriteLine("This is a console app with the Leet Code exercise \nPracticing them and implementing in a program");
