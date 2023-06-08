6a84f18eb401dec78499dcdda74a939dd12d5579
RobertGheo <robert.bradu@yahoo.com>
2023-06-08 14:04:29 +0300

    update exercise 1

---
+++ DifficultyEasy/A1_TwoSum.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetC_exercise.DifficultyEasy
+{
+    internal class A1_TwoSum
+    {
+        public static void mMain()
+        {   
+            int[] nums = { 2, 3, 7, 5, 11,19, 15 };
+            int target = 9;
+            int[] resTS = TwoSumB(nums, target);
+
+            Console.Write("[ ");
+            foreach (int i in resTS)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine("]");
+        }
+        
+        //First solution brute force
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = { };
+            for(int i=0; i< nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }else if (nums[i] + nums[j] == target)
+                    {
+                        result = result.Append(i).ToArray();
+                    }
+                }
+            }
+            return result;
+        }
+        //Second solution, using dictionary
+        public static int[] TwoSumB(int[] nums, int target)
+        {
+            Dictionary<int, int> hashD = new Dictionary<int, int>();
+            
+            for (int i =0; i < nums.Length; i++) 
+            { 
+                int check = target - nums[i];
+                if (hashD.ContainsKey(check))
+                {
+                    return new int[] { hashD[check], i };
+                }
+                else if (!hashD.ContainsKey(nums[i])) 
+                {
+                    hashD.Add(nums[i], i);
+                }
+            }
+            return null;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LeetC_exercise.DifficultyEasy;
+
 Console.WriteLine("Hello there \n");
-Console.WriteLine("This is a console app with the Leet Code exercise \nPracticing them and implementing in a program");
+Console.WriteLine("This is a console app with the Leet Code exercise \nPracticing them and implementing in a program\n");
+
+bool quitVar = false;
+while (quitVar == false)
+{
+    Console.Write("\nSelect a menu option: ");
+    string inputMenu = Console.ReadLine().Trim().ToLower();
+
+    switch (inputMenu)
+    {
+        case "a1":
+            A1_TwoSum.mMain();
+            break;
+
+
+
+        case "q":
+            quitVar = true;
+            break;
+        default:
+            Console.WriteLine("\tWrong input, please insert a menu option");
+            break;
+    }
+}
+ 
+
+
